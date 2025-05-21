956a6f722bf2530e8f664217e03ca9035846588a
Admin <olexandr.11.f@gmail.com>
2025-05-21 07:18:07 +0300

    change url for deploy

--- react-web_lab/settings.ts
+++ react-web_lab/settings.ts
@@ -1,5 +1,6 @@
 
-const beckEndServerUrl = 'http://localhost:3000';
+// const beckEndServerUrl = 'http://localhost:3000';
+const beckEndServerUrl = "https://web-lab5-back-4owx.onrender.com/";
 
 export { beckEndServerUrl };
 
