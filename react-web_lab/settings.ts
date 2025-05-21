b7544da2873e2aa6847988b848b354b3a8fe7b1d
Admin <olexandr.11.f@gmail.com>
2025-05-21 11:02:49 +0300

    fix backend URL

--- react-web_lab/settings.ts
+++ react-web_lab/settings.ts
@@ -1,6 +1,6 @@
 
 // const beckEndServerUrl = 'http://localhost:3000';
-const beckEndServerUrl = "https://web-lab5-back-4owx.onrender.com/";
+const beckEndServerUrl = "https://web-lab5-back-4owx.onrender.com";
 
 export { beckEndServerUrl };
 
